43a03e748a9424f252e390b024a277251ee2e09e
Sergey Grabak <seventyseventh@gmail.com>
2023-04-29 16:32:22 +0200

    refactor: change spaces count

--- src/init.js
+++ src/init.js
@@ -3,10 +3,10 @@
 import VirtualKeyboard from './components/VirtualKeyboard.js';
 
 export default () => {
-  const body = document.body;
-  const root = document.createElement('div');
-  body.appendChild(root);
+    const { body } = document;
+    const root = document.createElement('div');
+    body.appendChild(root);
 
-  const obj = new VirtualKeyboard(root);
-  obj.init();
+    const obj = new VirtualKeyboard(root);
+    obj.init();
 };
